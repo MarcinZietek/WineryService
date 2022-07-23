acd50a7be755344190929404f04e60787c78dddb
MarcinZietek <marcin.it.zietek@gmail.com>
2022-07-23 11:50:54 +0200

    added wine controller #2

---
+++ winery_service/src/main/java/pl/mz/winery_service/controller/WineController.java
@@ -0,0 +1,28 @@
+package pl.mz.winery_service.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.mz.winery_service.model.WineDto;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/wine")
+@RestController
+public class WineController {
+
+    @GetMapping({"/{wineId}"})
+    public ResponseEntity<WineDto> getWineById(@PathVariable("wineId") UUID wineId){
+        return new ResponseEntity<>(WineDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewWine(@RequestBody WineDto wineDto){
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{wineId}"})
+    public ResponseEntity updateWineId(@PathVariable("wineId") UUID wineId, RequestBody wineDto){
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
