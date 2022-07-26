eca57e447af5abb4a2dabd90945e78385644c49b
MarcinZietek <marcin.it.zietek@gmail.com>
2022-07-26 11:43:53 +0200

    added wine entity #5

---
+++ winery_service/src/main/java/pl/mz/winery_service/domain/Wine.java
@@ -0,0 +1,37 @@
+package pl.mz.winery_service.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Wine {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+    @Version
+    private Long version;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+    private String wineName;
+    private String wineStyle;
+    private BigDecimal price;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+}
